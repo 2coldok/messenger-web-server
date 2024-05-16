3b80b7eeba01c5ba1f84181428fc94fedc04a02e
coldok <2coldok@gmail.com>
2024-05-17 01:15:05 +0900

    chore : 경로 변경

---
+++ middleware/validator.js
@@ -0,0 +1,20 @@
+import { validationResult } from "express-validator"
+
+export const validate = (req, res, next) => {
+  const errors = validationResult(req);
+  if (errors.isEmpty()) {
+    return next();
+  }
+  return res.status(400).json({ message: errors.array()[0].msg });
+}
+
+// res.status(400).json({ message: errors.array() }) 의 생김새는 다음과 같다
+// "message": [
+//   {
+//       "type": "field",
+//       "value": "메세",
+//       "msg": "text는 최소 3글자 이상",
+//       "path": "text",
+//       "location": "body"
+//   }
+// ]
--- router/tweets.js
+++ router/tweets.js
@@ -1,23 +1,33 @@
-import 'express-async-errors';
-import express from 'express';
-import * as tweetController from '../controller/tweet.js';
+import "express-async-errors";
+import express from "express";
+import * as tweetController from "../controller/tweet.js";
+import { body } from "express-validator";
+import { validate } from "../middleware/validator.js";
 
 const router = express.Router();
 
+const validateTweet = [
+  body("text")
+    .trim()
+    .isLength({ min: 3 })
+    .withMessage("text는 최소 3글자 이상"),
+  validate,  
+];
+
 // GET /tweets : 모든 tweet들을 반환한다.
 // GET /tweets?username=:username : username에 해당하는 tweet들을 반환한다.
-router.get('/', tweetController.getTweets);
+router.get("/", tweetController.getTweets);
 
 // GET /tweets/:id
-router.get('/:id', tweetController.getTweet);
+router.get("/:id", tweetController.getTweet);
 
 // POST /tweets
-router.post('/', tweetController.createTweet);
+router.post("/", validateTweet, tweetController.createTweet);
 
 // PUT /tweets/:id
-router.put('/:id', tweetController.updateTweet);
+router.put("/:id", validateTweet, tweetController.updateTweet);
 
 // DELETE /tweets/:id
-router.delete('/:id', tweetController.deleteTweet);
+router.delete("/:id", tweetController.deleteTweet);
 
 export default router;
